451be97f9a382546ec9ffc8be7f79052e9b29d6b
Amit Vikram Raj <82011740+avr2002@users.noreply.github.com>
2024-07-18 04:00:16 +0000

    feat: populated the repository from the `python-cookiecutter-template`

---
+++ tests/unit_tests/__init__.py
@@ -0,0 +1,11 @@
+"""
+Unit tests for serverless-image-editor.
+
+This folder ideally has a parallel folder structure with the
+`src/editor/` folder.
+
+In general, unit tests
+
+- should be fast and test a single function or class
+- should not depend on external resources (e.g., databases, network, etc.).
+"""
---
+++ tests/unit_tests/test_states_info.py
@@ -0,0 +1,30 @@
+"""Tests for `editor.states_info`."""
+
+import pytest
+
+from editor.states_info import (
+    is_city_capital_of_state,
+    slow_add,
+)
+
+
+# multiple tests in one
+@pytest.mark.parametrize(
+    argnames="city_name, state, is_capital",
+    argvalues=[
+        ("Ranchi", "Jharkhand", True),
+        ("Patna", "Bihar", True),
+        ("Bangalore", "Karnataka", True),
+        ("Mumbai", "Maharashtra", True),
+        ("Patna", "Jharkhand", False),
+    ],
+)
+def test__is_city_capital_of_state(city_name: str, state: str, is_capital: bool):
+    """Assert `is_city_capital_of_state()` return correct answer for given city and state pairs."""
+    assert is_city_capital_of_state(city_name=city_name, state=state) == is_capital
+
+
+@pytest.mark.slow
+def test__slow_add():
+    """Test `slow_add()`."""
+    assert slow_add(1, 2) == 3
